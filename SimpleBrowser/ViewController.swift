4aa381a04cb387fbb11f02da3db6160bd548b89e
awang1 <awang1@andrew.cmu.edu>
2016-09-15 18:06:13 -0400

    Initial Commit

---
+++ SimpleBrowser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleBrowser
+//
+//  Created by Alex Wang on 9/15/16.
+//  Copyright © 2016 Alex Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
